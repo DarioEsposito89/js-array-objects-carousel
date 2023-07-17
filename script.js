2c4ea74e80bfce542c9553f1645af84f717a5591
DarioEsposito89 <darioesposito.work@gmail.com>
2023-07-17 18:57:55 +0200

    fatto cicli per left e right slider

--- script.js
+++ script.js
@@ -30,31 +30,59 @@
 
 const images = [
     {
-        image: 'img/01.webp',
+        image: '../img/01.webp',
         title: 'Marvel\'s Spiderman Miles Morale',
         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
     }, {
-        image: 'img/02.webp',
+        image: '../img/02.webp',
         title: 'Ratchet & Clank: Rift Apart',
         text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
     }, {
-        image: 'img/03.webp',
+        image: '../img/03.webp',
         title: 'Fortnite',
         text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
     }, {
-        image: 'img/04.webp',
+        image: '../img/04.webp',
         title: 'Stray',
         text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
     }, {
-        image: 'img/05.webp',
+        image: '../img/05.webp',
         title: "Marvel's Avengers",
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
 
-const leftContainer = document.getElementById("left-container");
-const rightContainer = document.getElementById("right-container");
+const leftSlider = document.getElementById("left-slider");
+const rightSlider = document.getElementById("right-slider");
 const infoTitle = document.getElementById("info-title");
 const infoText = document.getElementById("info-text");
 infoTitle.innerHTML = images[0].title;
 infoText.innerHTML = images[0].text;
+
+//CICLO CHE LEGGE LE IMMAGINI NEL SLIDER LEFT
+
+for (i = 0; i < images.length; i++) {
+    const singleImg = document.createElement("img");
+    singleImg.src = `img/${images[i].image}`;
+    singleImg.classList.add("left-slider-img");
+
+    if (i === 0) {
+        singleImg.classList.add("active");
+    }
+
+    leftSlider.append(singleImg);
+}
+
+// CICLO CHE LEGGE LE IMMAGINI NEL SLIDER RIGHT
+
+for (i = 0; i < images.length; i++) {
+    const singleImg = document.createElement("img");
+    singleImg.src = `img/${images[i].image}`;
+    singleImg.classList.add("right-slider-img");
+
+    if (i === 0) {
+        singleImg.classList.add("selected");
+    }
+    rightSlider.append(singleImg);
+}
+
